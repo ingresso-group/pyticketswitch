f0162387a3df3de0dc8f9ead1e09e4068e082396
Ross Gray <rossgray@ingresso.co.uk>
2015-03-13 12:34:05 +0000

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyticketswitch',
-    version='1.4.12',
+    version='1.4.13',
     author='Matt Jared',
     author_email='mattjared@ingresso.co.uk',
     packages=[
