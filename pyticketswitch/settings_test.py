99be517b4bad78d9e1aeb05b1b1bcd6aa8e56493
Ross Gray <rossgray@ingresso.co.uk>
2015-08-07 11:42:17 +0100

    Point tests at IPv4 externally-accessible dev API server

--- pyticketswitch/settings_test.py
+++ pyticketswitch/settings_test.py
@@ -1,7 +1,7 @@
 from settings import *
 
-API_URL = 'https://dogbert.ingresso.co.uk/cgi-bin/xml_core.exe'
-EXT_START_SESSION_URL = 'https://dogbert.ingresso.co.uk/cgi-bin/xml_start_session.exe'
+API_URL = 'https://dogbex.ingresso.co.uk/cgi-bin/xml_core.exe'
+EXT_START_SESSION_URL = 'https://dogbex.ingresso.co.uk/cgi-bin/xml_start_session.exe'
 
 TEST_USERNAME = 'whitelabel-testuser'
 TEST_PASSWORD = ''
