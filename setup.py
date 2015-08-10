766f9d3082e0584c60f2e30c5f27244077f30e7d
Nic West <nicwest@ingresso.co.uk>
2015-08-10 15:38:33 +0100

    update license and setup files

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pyticketswitch',
-    version='1.6.3',
+    version='1.6.4',
     author='Ingresso',
     author_email='systems@ingresso.co.uk',
     packages=[
         'pyticketswitch',
-        'pyticketswitch.test',
         'pyticketswitch.interface_objects'
     ],
-    license='LICENSE.txt',
+    license='MIT',
     description='A Python interface for the Ingresso XML Core API',
     long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
     ],
 )
