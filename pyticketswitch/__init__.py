e4c8c0e0452e83ae6c537155084156f512f4a05d
Nic West <nicwest.cb@gmail.com>
2016-04-29 11:34:47 +0100

    bump version number

--- pyticketswitch/__init__.py
+++ pyticketswitch/__init__.py
@@ -4,4 +4,4 @@ from __future__ import absolute_import, division, print_function
 from .interface_objects import *  # NOQA
 from .util import auto_date_to_slug, slug_to_auto_date  # NOQA
 
-__version__ = '1.10.0'
+__version__ = '1.10.1'
