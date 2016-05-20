7bac938a905af9321c5516d98127f4d50fff5d3c
Gordon <gordon@yplanapp.com>
2016-05-20 16:06:08 +0100

    lint

--- pyticketswitch/interface_objects/__init__.py
+++ pyticketswitch/interface_objects/__init__.py
@@ -3,15 +3,17 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-from .availability import AvailDetail, Concession, DespatchMethod, TicketType
-from .base import Address, Card, Commission, Currency, Customer, Seat
-from .bundle import Bundle
-from .core import Core
-from .event import Category, Event, Review, Video
-from .order import Order
-from .performance import Performance
-from .reservation import Reservation
-from .trolley import Trolley
+from .availability import (  # noqa
+    AvailDetail, Concession, DespatchMethod, TicketType
+)
+from .base import Address, Card, Commission, Currency, Customer, Seat  # noqa
+from .bundle import Bundle  # noqa
+from .core import Core  # noqa
+from .event import Category, Event, Review, Video  # noqa
+from .order import Order  # noqa
+from .performance import Performance  # noqa
+from .reservation import Reservation  # noqa
+from .trolley import Trolley  # noqa
 
 __all__ = [str(x) for x in (
     'Core', 'Category', 'Event', 'Review', 'Performance',
