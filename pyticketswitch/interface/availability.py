5b293b0af236aa2a26a9862cf624616fb0640380
Sam Holmes <samholmesdev@gmail.com>
2016-08-16 15:59:48 +0100

    WIP

--- pyticketswitch/interface/availability.py
+++ pyticketswitch/interface/availability.py
@@ -5,8 +5,8 @@ from pyticketswitch.interface.currency import Currency
 class Availability(object):
 
     def __init__(self, valid_quantity_mask=0, day_mask=0, currency=None,
-                 first_date=None, last_date=None, seatprice=0,
-                 surcharge=0):
+                 first_date=None, last_date=None, specific_dates=None,
+                 seatprice=0, surcharge=0):
 
         self.valid_quantity_mask = valid_quantity_mask
         self.day_mask = day_mask
@@ -15,6 +15,7 @@ class Availability(object):
         self.seatprice = seatprice
         self.surcharge = surcharge
         self.currency = currency
+        self.specific_dates = specific_dates
 
     @classmethod
     def from_api_data(cls, data):
@@ -28,7 +29,18 @@ class Availability(object):
         api_last_date = dates.get('last_yyyymmdd', None)
         first_date = None
         last_date = None
+        specific_dates = []
 
+        for year, value in dates.items():
+            if year.startswith('year'):
+                yyyy = year.split('year_')[1]
+                for month, mask in value.items():
+                    binary_mask = bin(mask)
+                    for day in xrange(0, 30):
+                        day_binary = bin(day)
+                        if day_binary & binary_mask:
+                            specific_dates.append(datetime.date(
+                                year=yyyy, month=month, day=day+1))
         if api_first_date:
             first_date = datetime.datetime.strptime(api_first_date, '%Y%m%d').date()
         if api_last_date:
@@ -40,6 +52,7 @@ class Availability(object):
             'currency': currency,
             'first_date': first_date,
             'last_date': last_date,
+            'last_date': specific_dates,
             'seatprice': data.get('seatprice', 0),
             'surcharge': data.get('surcharge', 0),
         }
