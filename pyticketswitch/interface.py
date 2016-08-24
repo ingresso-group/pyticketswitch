74f6b48e3bd56815beb6e2afd811e4d3d381612f
Sam Holmes <samholmesdev@gmail.com>
2016-08-24 11:50:14 +0100

    WIP

--- pyticketswitch/interface.py
+++ pyticketswitch/interface.py
@@ -419,6 +419,8 @@ class CoreAPI(object):
             self, crypto_block, upfront_data_token=None, discount_token=None,
             despatch_token=None):
         print(discount_token)
+        print('crypto')
+        print(crypto_block)
         resp = self.make_core_request(
             'create_order',
             crypto_block=crypto_block, upfront_data_token=upfront_data_token,
