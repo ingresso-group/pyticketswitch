961997894f4308872bac74b6b8803661d0dc1e9f
Nic West <nicwest.cb@gmail.com>
2016-12-14 14:26:59 +0000

    add country and send method objects

---
+++ pyticketswitch/interface/country.py
@@ -0,0 +1,14 @@
+class Country(object):
+
+    def __init__(self, code, description=None):
+        self.code = code
+        self.description = description
+
+    @classmethod
+    def from_api_data(cls, data):
+        kwargs = {
+            'code': data.get('country_code'),
+            'description': data.get('country_desc'),
+        }
+
+        return cls(**kwargs)
---
+++ pyticketswitch/interface/send_method.py
@@ -0,0 +1,38 @@
+from pyticketswitch.interface.country import Country
+
+
+class SendMethod(object):
+
+    def __init__(self, code, cost=None, description=None, typ=None,
+                 permitted_countries=None):
+        self.code = code
+        self.cost = cost
+        self.description = description
+        self.typ = typ
+        self.permitted_countries = permitted_countries
+
+    @classmethod
+    def from_api_data(cls, data):
+        kwargs = {
+            'code': data.get('send_code'),
+            'description': data.get('send_desc'),
+            'typ': data.get('send_type'),
+        }
+
+        cost = data.get('send_cost')
+        if cost:
+            kwargs.update(cost=float(cost))
+
+        permitted_countries_raw = data.get('permitted_countries', {})
+
+        if permitted_countries_raw:
+            permitted_countries_raw = permitted_countries_raw.get('country', [])
+
+            permitted_countries = [
+                Country.from_api_data(country)
+                for country in permitted_countries_raw
+            ]
+
+            kwargs.update(permitted_countries=permitted_countries)
+
+        return cls(**kwargs)
