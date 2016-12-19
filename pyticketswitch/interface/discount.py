e15dc000857dfa856e22419e3070ab366d2550d2
Nic West <nicwest.cb@gmail.com>
2016-12-19 11:44:08 +0000

    test discounts

--- pyticketswitch/interface/discount.py
+++ pyticketswitch/interface/discount.py
@@ -1,22 +1,42 @@
 class Discount(object):
 
-    def __init__(self, code, description=None, seatprice=None, surcharge=None,
-                 is_offer=False, non_offer_seatprice=None,
-                 non_offer_surcharge=None, available=None,
-                 percentage_saving=0, absolute_saving=0,
-                 disallowed_mask=0):
+    def __init__(self, code, description=None, price_band=None, seatprice=None,
+                 surcharge=None, is_offer=False, non_offer_seatprice=None,
+                 non_offer_surcharge=None, availability=None,
+                 percentage_saving=0, absolute_saving=0, disallowed_mask=0):
 
         self.code = code
         self.description = description
+        self.price_band = price_band
         self.is_offer = is_offer
         self.seatprice = seatprice
         self.surcharge = surcharge
         self.non_offer_seatprice = non_offer_seatprice
         self.non_offer_surcharge = non_offer_surcharge
-        self.available = available
+        self.availability = availability
         self.percentage_saving = percentage_saving
         self.absolute_saving = absolute_saving
         self.disallowed_mask = disallowed_mask
+
+    @classmethod
+    def from_api_data(cls, data):
+
+        kwargs = {
+            'code': data.get('discount_code'),
+            'description': data.get('discount_desc'),
+            'price_band': data.get('price_band_code'),
+            'is_offer': data.get('is_offer', False),
+            'seatprice': data.get('sale_seatprice'),
+            'surcharge': data.get('sale_surcharge'),
+            'non_offer_seatprice': data.get('non_offer_sale_seatprice'),
+            'non_offer_surcharge': data.get('non_offer_sale_surcharge'),
+            'availability': data.get('number_available'),
+            'percentage_saving': data.get('percentage_saving'),
+            'absolute_saving': data.get('absolute_saving'),
+            'disallowed_mask': data.get('discount_disallowed_seat_no_bitmask'),
+        }
+
+        return cls(**kwargs)
 
     def is_available(self, no_of_tickets=None, seat_number=None):
 
---
+++ tests/interface/test_discount.py
@@ -0,0 +1,33 @@
+from pyticketswitch.interface.discount import Discount
+
+
+class TestDiscount:
+
+    def test_from_api_data(self):
+        data = {
+            'discount_code': 'ADULT',
+            'discount_desc': 'Adult standard',
+            'price_band_code': 'A/pool',
+            'sale_seatprice': 160,
+            'sale_surcharge': 5.5,
+            'is_offer': True,
+            'non_offer_sale_seatprice': 200,
+            'non_offer_sale_surcharge': 5.5,
+            'absolute_saving': 40,
+            'percentage_saving': 20,
+            'number_available': 6,
+            'discount_disallowed_seat_no_bitmask': 126,
+        }
+
+        discount = Discount.from_api_data(data)
+
+        assert discount.code == 'ADULT'
+        assert discount.description == 'Adult standard'
+        assert discount.price_band == 'A/pool'
+        assert discount.seatprice == 160.0
+        assert discount.surcharge == 5.5
+        assert discount.is_offer is True
+        assert discount.non_offer_seatprice == 200
+        assert discount.non_offer_surcharge == 5.5
+        assert discount.availability == 6
+        assert discount.disallowed_mask == 126
