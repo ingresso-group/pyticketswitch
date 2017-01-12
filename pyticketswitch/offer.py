0c0c12c1aa84c7b4d1f387086d426e2fdc6dfe1c
Nic West <nicwest.cb@gmail.com>
2017-01-12 10:21:43 +0000

    fix offer

--- pyticketswitch/offer.py
+++ pyticketswitch/offer.py
@@ -1,8 +1,8 @@
 class Offer(object):
 
-    def __init__(self, absolute_saving=0, percentage_saving=0,
-                 original_seatprice=0, original_surcharge=0,
-                 seatprice=0, surcharge=0):
+    def __init__(self, absolute_saving=None, percentage_saving=None,
+                 original_seatprice=None, original_surcharge=None,
+                 seatprice=None, surcharge=None):
 
         self.absolute_saving = absolute_saving
         self.percentage_saving = percentage_saving
@@ -14,11 +14,11 @@ class Offer(object):
     @classmethod
     def from_api_data(cls, data):
         kwargs = {
-            'absolute_saving': data.get('absolute_saving', 0),
-            'percentage_saving': data.get('percentage_saving', 0),
-            'original_seatprice': data.get('original_seatprice', 0),
-            'original_surcharge': data.get('original_surcharge', 0),
-            'seatprice': data.get('seatprice', 0),
-            'surcharge': data.get('surcharge', 0),
+            'absolute_saving': data.get('absolute_saving'),
+            'percentage_saving': data.get('percentage_saving'),
+            'original_seatprice': data.get('full_seatprice'),
+            'original_surcharge': data.get('full_surcharge'),
+            'seatprice': data.get('offer_seatprice'),
+            'surcharge': data.get('offer_surcharge'),
         }
         return cls(**kwargs)
