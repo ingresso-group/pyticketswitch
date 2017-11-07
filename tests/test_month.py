8c9331bed94ad4f0af1789ce16bfbe9055b39d7a
Sam Holmes <samholmesdev@gmail.com>
2017-11-07 12:04:41 +0000

    Add basic tests

--- tests/test_month.py
+++ tests/test_month.py
@@ -11,6 +11,22 @@ class TestMonth:
             'month_dates_bitmask': 1065254912,
             'month_desc': 'November',
             'month_weekdays_bitmask': 63,
+            'cost_range': {
+                'range_currency_code': 'gbp',
+                'valid_quantities': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                'max_surcharge': 39.45,
+                'max_seatprice': 197.25,
+                'min_surcharge': 5.4,
+                'min_seatprice': 20.0,
+                'no_singles_cost_range': {
+                    'range_currency_code': 'gbp',
+                    'valid_quantities': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    'max_surcharge': 39.45,
+                    'max_seatprice': 197.25,
+                    'min_surcharge': 5.4,
+                    'min_seatprice': 30.0
+                },
+            }
         }
 
         month = Month.from_api_data(data)
@@ -20,6 +36,29 @@ class TestMonth:
         assert month.description == 'November'
         assert month._dates_bitmask == 1065254912
         assert month._weekday_bitmask == 63
+        assert month.cost_range.min_seatprice == 20.00
+        assert month.cost_range.currency == 'gbp'
+        assert month.no_singles_cost_range.min_seatprice == 30.00
+        assert month.cost_range.currency == 'gbp'
+
+    def test_from_api_data_without_cost_range(self):
+        data = {
+            'month': 'nov',
+            'year': 2018,
+            'month_dates_bitmask': 1065254912,
+            'month_desc': 'November',
+            'month_weekdays_bitmask': 63
+        }
+
+        month = Month.from_api_data(data)
+
+        assert month.month == 11
+        assert month.year == 2018
+        assert month.description == 'November'
+        assert month._dates_bitmask == 1065254912
+        assert month._weekday_bitmask == 63
+        assert month.cost_range is None
+        assert month.no_singles_cost_range is None
 
     def test_is_available(self):
         # 1065254912 == 111111011111101000000000000000
