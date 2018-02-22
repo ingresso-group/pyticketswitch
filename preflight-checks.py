e9458846cf746b8f06518ab80ee239489e9f6188
Nic West <nicwest.cb@gmail.com>
2018-02-22 17:33:38 +0000

    fix formatting of preflights

--- preflight-checks.py
+++ preflight-checks.py
@@ -5,18 +5,18 @@ import re
 if __name__ == '__main__':
 
     # check all the version numbers are consistent.
-    print('pyticketswitch/init.py:', pyticketswitch.__version__)
-    print('docs/conf.py (version): ', docs.conf.version)
-    print('docs/conf.py (release): ', docs.conf.release)
+    print('pyticketswitch/init.py: {}'.format(pyticketswitch.__version__))
+    print('docs/conf.py (version): {}'.format(docs.conf.version))
+    print('docs/conf.py (release): {}'.format(docs.conf.release))
 
     setup = None
     with open('setup.py', 'r') as fh:
         match = re.search(r"\s*version\s*=\s*'([^']+)'", fh.read())
         if match:
             setup = match.group(1)
-            print('setup.py: ', setup)
+            print('setup.py: {}'.format(setup))
         else:
-            print('version info missing from setup.py')
+            print('setup.py: MISSING!')
 
     all_match = all(
         version == pyticketswitch.__version__
