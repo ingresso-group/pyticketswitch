56d430dca7f8ef682e19840b9c26974967092b40
Shannon Kelly <shannonkelly955@hotmail.co.uk>
2023-04-25 14:48:55 +0100

    prepare version 2.14.0 (#122)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pyticketswitch',
-    version='2.13.0',
+    version='2.14.0',
     author='Ingresso',
     author_email='systems@ingresso.co.uk',
     url='https://github.com/ingresso-group/pyticketswitch/',
