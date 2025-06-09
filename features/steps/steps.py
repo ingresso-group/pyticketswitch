e242bc8ba9ee2a758fc011de467f7064274bd98c
joegrimer <74649399+joegrimer@users.noreply.github.com>
2025-06-09 11:39:45 +0100

    Improve Cost Range class (#125)
    * Improve Cost Range class
    
    * Fix authentication changes
    
    * Flake
    
    * tests/test_user.py
    
    * pyticketswitch/user.py,tests/test_user.py
    
    * Dont expect user in test.v1
    
    * Make it an enum

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,25 +1,17 @@
-from behave import given, then
-from hamcrest import (
-    assert_that, instance_of
-)
+from behave import given
 from pyticketswitch import Client
-from pyticketswitch.exceptions import AuthenticationError
 
 
 @given('an API client with valid credentials')
 def given_an_api_client_with_valid_credentials(context):
     client = Client('demo', 'demopass', url='https://api.ticketswitch.com')
     context.client = client
-    context.user_id = 'demo'
+    context.expected_user_id = 'demo'
 
 
 @given('an API client with invalid credentials')
 def given_an_api_client_with_invalid_credentials(context):
     client = Client('boatymcboatyface', 'irship', url='https://api.ticketswitch.com')
     context.client = client
-    context.user_id = 'boatymcboatyface'
+    context.expected_user_id = 'boatymcboatyface'
 
-
-@then('an authorisation error should be raised')
-def then_the_response_should_contain_a_user_object(context):
-    assert_that(context.exception, instance_of(AuthenticationError))
